f9bd6ff664bf422934aeb7d18f29dcf958833094
hbogaeus <henry.bogaeus@gmail.com>
2022-12-11 21:20:42 +0100

    Completed day 8 problem 2

--- src/main/kotlin/Day8.kt
+++ src/main/kotlin/Day8.kt
@@ -5,9 +5,84 @@ fun main() {
 
     val grid = parseTrees(input)
 
+    problem2(grid)
+}
+private fun problem2(
+    grid: List<List<Tree>>
+) {
     val gridHeight = grid.size
     val gridWidth = grid[0].size
 
+    var maxScenicScore = 0
+
+    for (y in 0 until gridHeight - 1) {
+        for (x in 0 until gridWidth - 1) {
+            val currentTree = grid[y][x]
+
+            var visibleTreesToTheLeft = 0
+
+            // Check left
+            for (z in x - 1 downTo 0) {
+                visibleTreesToTheLeft += 1
+                val tree = grid[y][z]
+
+                if (tree.height >= currentTree.height) {
+                    break
+                }
+            }
+
+            var visibleTreesFromTheRight = 0
+
+            // Check right
+            for (z in x + 1 until gridWidth) {
+                visibleTreesFromTheRight += 1
+                val tree = grid[y][z]
+
+                if (tree.height >= currentTree.height) {
+                    break
+                }
+            }
+
+            var visibleTreesFromUp = 0
+
+            // Check up
+            for (z in y - 1 downTo 0) {
+                visibleTreesFromUp += 1
+                val tree = grid[z][x]
+
+                if (tree.height >= currentTree.height) {
+                    break
+                }
+            }
+
+            var visibleTreesFromDown = 0
+
+            // Check down
+            for (z in y + 1 until gridHeight) {
+                visibleTreesFromDown += 1
+                val tree = grid[z][x]
+
+                if (tree.height >= currentTree.height) {
+                    break
+                }
+            }
+
+            val scenicScore = visibleTreesToTheLeft * visibleTreesFromTheRight * visibleTreesFromUp * visibleTreesFromDown
+
+            if (scenicScore > maxScenicScore) {
+                maxScenicScore = scenicScore
+            }
+        }
+    }
+
+    println(maxScenicScore)
+}
+
+private fun problem1(
+    grid: List<List<Tree>>
+) {
+    val gridHeight = grid.size
+    val gridWidth = grid[0].size
     var visibleTrees = gridHeight * 2 + gridWidth * 2 - 4
 
     for (y in 1 until gridHeight - 1) {
@@ -30,7 +105,7 @@ fun main() {
             var isVisibleFromRight = true
 
             // Check right
-            for (z in x + 1 until  gridWidth) {
+            for (z in x + 1 until gridWidth) {
                 val tree = grid[y][z]
 
                 if (tree.height >= currentTree.height) {
@@ -54,7 +129,7 @@ fun main() {
             var isVisibleFromDown = true
 
             // Check down
-            for (z in y + 1 until  gridHeight) {
+            for (z in y + 1 until gridHeight) {
                 val tree = grid[z][x]
 
                 if (tree.height >= currentTree.height) {
